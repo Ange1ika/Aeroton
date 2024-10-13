5aae5cb1e0e2477901ee2e07ebb68468e5bdce6a
Kochetkova Ange1ika <kochetkova.angelika@yandex.ru>
2024-10-13 17:21:57 +0300

    ¨New

--- sketch_oct9a.ino/sketch_oct9a.ino.ino
+++ sketch_oct9a.ino/sketch_oct9a.ino.ino
@@ -17,39 +17,52 @@
 
 // # Working Mode: PWM trigger pin  mode.
 
-int URECHO = 3;         // PWM Output 0-50000US,Every 50US represent 1cm
-int URTRIG = 5;         // trigger pin
+int URECHO = 3;         // PWM Output 0-50000US, Every 50US represents 1cm
+int URTRIG = 5;         // Trigger pin
 
 unsigned int DistanceMeasured = 0;
+unsigned long lastTime = 0;  // Время последней публикации
+unsigned long interval = 0;   // Интервал между публикациями
+unsigned long count = 0;      // Счетчик публикаций
 
-void setup()
-{
-  //Serial initialization
-  Serial.begin(9600);                        // Sets the baud rate to 9600
-  pinMode(URTRIG, OUTPUT);                   // A low pull on pin COMP/TRIG
-  digitalWrite(URTRIG, HIGH);                // Set to HIGH
-  pinMode(URECHO, INPUT);                    // Sending Enable PWM mode command
-  delay(500);
+void setup() {
+  // Serial initialization
+  Serial.begin(9600);                       // Sets the baud rate to 9600
+  pinMode(URTRIG, OUTPUT);                  // A low pull on pin COMP/TRIG
+  digitalWrite(URTRIG, HIGH);               // Set to HIGH
+  pinMode(URECHO, INPUT);                   // Sending Enable PWM mode command
+  delay(5);
   Serial.println("Init the sensor");
-
 }
-void loop()
-{
+
+void loop() {
   Serial.print("Distance=");
   digitalWrite(URTRIG, LOW);
   digitalWrite(URTRIG, HIGH);              
 
-  unsigned long LowLevelTime = pulseIn(URECHO, LOW) ;
-  if (LowLevelTime >= 50000)              // the reading is invalid.
-  {
+  unsigned long LowLevelTime = pulseIn(URECHO, LOW);
+  if (LowLevelTime >= 50000) {              // The reading is invalid.
     Serial.println("Invalid");
-  }
-  else
-  {
-    DistanceMeasured = LowLevelTime / 50;  // every 50us low level stands for 1cm
+  } else {
+    DistanceMeasured = LowLevelTime / 50;   // Every 50us low level stands for 1cm
     Serial.print(DistanceMeasured);
-    Serial.println("cm");
+    Serial.println(" cm");
   }
 
-  delay(200);
+  // Вычисление и публикация частоты
+  unsigned long currentTime = millis(); // Текущее время в миллисекундах
+  interval = currentTime - lastTime;     // Интервал между публикациями
+  count++;                                // Увеличиваем счетчик
+
+  if (interval >= 1000) { // Если прошло более 1 секунды
+    Serial.print("Frequency = ");
+    Serial.print(count);          // Публикация количества сообщений
+    Serial.println(" messages/second");
+    
+    // Сбрасываем счетчик и время
+    count = 0; 
+    lastTime = currentTime;
+  }
+
+  delay(10); // Задержка для контроля частоты измерений
 }
